6aa1ae13c487220192aeb9b54b60d80706be3219
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-02 22:42:52 +0530

    Explained the conditional statements in Thymeleaf: elvis operator, if-unless, and switch-case.

--- Video 031 Conditional statement in Thymeleaf/MyController.java
+++ Video 031 Conditional statement in Thymeleaf/MyController.java
@@ -11,30 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-public class MyController {
-	@RequestMapping(value="/about", method=RequestMethod.GET)
-	public String about(Model model)
-	{
-		System.out.println("Going to the about page...");
-		
-		// Sending data to the template.
-		
-		model.addAttribute("name","Anuprash Gautam");
-		model.addAttribute("currentDate", new Date().toLocaleString());
-		
-		return "about";
-	}
-	
-	@GetMapping("/example-loop")
-	public String iterateHandler(Model model)
-	{
-		//Sending the object, to which we want to traverse with the help of loop.
-		List<String> names = List.of("Ankit","Laxmi","John","Karan");
-		model.addAttribute("list",names);
-		
-		return "iterate";
-	}
-	
+public class MyController {	
 	@GetMapping("/condition")
 	public String conditionHandler(Model model)
 	{
