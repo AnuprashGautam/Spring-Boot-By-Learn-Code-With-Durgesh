b00b183957c383404758622e2e1a3e7192da9637
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-02 23:26:25 +0530

    Passing the dynamic value from controller to service and then finally to footer1 fragment.

---
+++ Video 033 Thymeleaf passing dynamic value to template while including/MyController.java
@@ -0,0 +1,25 @@
+package com.practice.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class MyController {
+	@GetMapping("/service")
+	public String serviceHandler(Model model)
+	{
+		// Sending dynamic data to the service and then to footer.
+		
+		model.addAttribute("title","This is title from controller.");
+		model.addAttribute("subTitle","This is sub title form controller.");
+		
+		return "service";
+	}
+}
