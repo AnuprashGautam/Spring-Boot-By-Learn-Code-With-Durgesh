37910502fb34978e01f847349c1df60933b66b56
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-09 19:59:56 +0530

    Did authentication and autherization using Spring Security.

---
+++ Video 048💡Spring security login system/config/MyConfig.java
@@ -0,0 +1,57 @@
+package com.smart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MyConfig {
+
+	// It helps in encoding the password so that if a person get access to the DB, can't use the user profile.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    // Provides the user details.
+    @Bean 
+    public UserDetailsService getUserDetailsService() {
+    	return new UserDetailsServiceImpl();
+    }
+    
+    // It authenticate the user by using the getUserDetailsService, and passwordEncoder beans.
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+    	DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+    	
+    	daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
+    	daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+    	
+    	return daoAuthenticationProvider;
+    }
+    
+    
+    // Autherization based on the http request is managed here.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf(csrf -> csrf.disable())  // Disable CSRF
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/user/**").hasRole("USER")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/**").permitAll()
+            )
+            //.formLogin(Customizer.withDefaults())  // Default Spring Security login form
+            .formLogin(form -> form
+                    .loginPage("login")   // Specify your custom login page URL
+                    .permitAll()           // Allow all users to access the login page
+                )
+            .build();
+    }
+}
